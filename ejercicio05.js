9a99437d2fb8826fbb551449fe08d309c5faa21e
fabio <ing.fabio.arg@gmail.com>
2024-07-03 21:02:20 -0300

    Resolucion

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,3 +1,12 @@
 // Ejercicio 5
 // Imprimir los números pares desde N (siendo N un número par que se lee) en forma descendente
 // hasta 2.
+const prompt = require('prompt-sync')({ siging: true }) // Paso adicional: npm i prompt-sync
+
+let N = parseInt(prompt('Ingrese el primer numero: '))
+if (N % 2 !== 0) N--
+// N = (N % 2 !== 0) ?  N-- : N
+
+for (let i = N; i >= 2; i -= 2) {
+  console.log({ i })
+}
